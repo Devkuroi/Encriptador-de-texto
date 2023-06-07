b11ce8939b0b8b7283b795faa080b6505c4af523
Devkuroi <joan2000oviedo@gmail.com>
2023-06-06 22:12:34 -0500

    LocalStorage add

--- index.js
+++ index.js
@@ -74,4 +74,18 @@ const dark_mode = document.querySelector(".switch")
 dark_mode.addEventListener("click", () => {
     dark_mode.classList.toggle("active-dark")
     document.body.classList.toggle("dark")
+
+	if(document.body.classList.contains("dark")) {
+		localStorage.setItem("dark-mode", "true")
+	} else {
+		localStorage.setItem("dark-mode", "false")
+	}
 })
+
+if(localStorage.getItem("dark-mode") === "true") {
+	document.body.classList.add("dark")
+	dark_mode.classList.add("active-dark")
+} else {
+	document.body.classList.remove("dark")
+	dark_mode.classList.remove("active-dark")
+}
